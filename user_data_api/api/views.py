6dd7ff03a02ba73564202a1efa3f4c1a61613ce9
siddheshgupte <siddhesh.gupte2468@gmail.com>
2019-03-13 13:56:10 -0400

    Add token authentication

--- user_data_api/api/views.py
+++ user_data_api/api/views.py
@@ -3,11 +3,14 @@ from .serializers import UserDataModelSerializer
 from .models import UserDataModel
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 
 class CreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
+
     queryset = UserDataModel.objects.all()
     serializer_class = UserDataModelSerializer
 
@@ -16,6 +19,8 @@ class CreateView(generics.ListCreateAPIView):
 
 
 class GetDataByNameView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, name, year):
         user_data = UserDataModel.objects.get(name=name, birth_date__contains=year)
         serializer = UserDataModelSerializer(user_data)
--- user_data_api/user_data_api/settings.py
+++ user_data_api/user_data_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4ly-i^#a2k2%1=eda)euqx8$en!ein$o&f6)_!==i$t*26$0ts'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
